236e484516b0ec6612dda09ca8abdd7335410b81
Dylan Staley <staley.dylan@gmail.com>
2014-10-07 15:45:22 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-realtime-hacker-news
+Realtime Hacker News
 ====================
 
-A realtime clone of the Hacker News homepage backed by HN's Firebase API
+This is a clone of the Hacker News homepage using Firebase to display data in realtime. It's built using Ember and emberFire, Firebase's Ember Data bindings.
+
+## Not Supported
+At this time, displaying comments isn't supported. Furthermore, the API is read-only, so you won't be able to vote or comment on stories.
+
+## License
+MIT
---
+++ js/app.js
@@ -0,0 +1,43 @@
+var App = Ember.Application.create();
+
+App.ApplicationAdapter = DS.FirebaseAdapter.extend({
+    firebase: new Firebase("https://hacker-news.firebaseio.com/v0")
+});
+
+App.Item = DS.Model.extend({
+    title: DS.attr('string'),
+    by: DS.attr('string'),
+    score: DS.attr('number'),
+    type: DS.attr('string'),
+    url: DS.attr('string'),
+    domain: function() {
+        var url_parts = (new URL(this.get('url')).hostname).split('.');
+        return '%@.%@'.fmt(url_parts[url_parts.length - 2], url_parts[url_parts.length - 1])
+    }.property('url'),
+    commentsUrl: function() {
+        return '//news.ycombinator.com/item?id=%@'.fmt(this.id)
+    }.property('id'),
+    authorUrl: function() {
+        return '//news.ycombinator.com/user?id=%@'.fmt(this.get('by'))
+    }.property('by'),
+    time: DS.attr('number'),
+    publishedDate: function() {
+        return moment.unix(this.get('time')).fromNow();
+    }.property('time'),
+});
+
+Ember.Inflector.inflector.uncountable('item');
+
+App.IndexRoute = Ember.Route.extend({
+    model: function() {
+        var route = this;
+        return new Ember.RSVP.Promise(function(resolve, reject) {
+            $.getJSON('https://hacker-news.firebaseio.com/v0/topstories.json', function(stories) {
+                for (var i = 0; i < 30 + 1; i++) {
+                    route.store.find('item', stories[i]);
+                }
+                resolve(route.store.all('item'));
+            });
+        });
+    }
+});
